7d95aafab5b07d8913e35a484f9bddad768a1211
kaiovbraga <kaiovbraga2001@gmail.com>
2021-09-11 18:04:22 -0300

    feat: Changing scroll hierarchy

--- src/components/Templates/Home/styles.ts
+++ src/components/Templates/Home/styles.ts
@@ -4,10 +4,13 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
   min-height: 100vh;
+  max-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   background-color: #181818;
+  overflow-y: scroll;
+  overflow-x: hidden;
 
   > section {
     min-width: 496px;
